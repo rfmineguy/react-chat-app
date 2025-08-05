8c8f3b876a9b395c178628fda2b9810cb5a07eb5
Riley Fischer <rfmineguy@gmail.com>
2025-08-05 08:59:02 -0700

    backend: Add function for adding a chatline to a joined room - if the user is not in a room, nothing happens - when the chatline is added successfully it broadcasts an update to   those who are connected to that room

--- chat-backend/connection_mgr.py
+++ chat-backend/connection_mgr.py
@@ -62,3 +62,23 @@ class ConnectionManager:
         for conn in self.connections:
             json_rooms = [json.dumps(room.tojson()) for room in self.rooms]
             await self.sio.emit('send-rooms', UpdateRoomsMessage(json_rooms).tojson(), conn)
+
+    async def addchatline(self, sid, chatline):
+        # Get the room this client is connected to
+        joined_rooms = [room for room in self.rooms if sid in room.users]
+
+        # If no sid is not in a room ignore the request (for now)
+        if len(joined_rooms) == 0:
+            print(f"[warn] User ${sid} not in a room")
+            return
+
+        # Add the chatline to the joined room
+        joined_room = joined_rooms[0]
+        await joined_room.add_chatline(sid, chatline)
+
+        # Send an update to the 'sid's in that room
+        for sid in joined_room.users:
+            await self.sio.emit('update-chat', UpdateChatlines(joined_room.chatlines).tojson(), sid)
+
+        print(f'adding chat to room: ${joined_room}')
+        pass
