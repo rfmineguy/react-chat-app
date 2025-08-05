3a9dae474dbecc7b160d4c1e020b94d0d50171b0
Riley Fischer <rfmineguy@gmail.com>
2025-08-05 08:26:43 -0700

    backend: Implement connect/disconnect for the connection manager

--- chat-backend/connection_mgr.py
+++ chat-backend/connection_mgr.py
@@ -8,3 +8,15 @@ class ConnectionManager:
         self.sio = sio
         self.connections: list[str] = []
         self.rooms: list[Room] = []
+
+    async def connect(self, sid: str):
+        self.connections.append(sid)
+        json_rooms = [json.dumps(room.tojson()) for room in self.rooms]
+        await self.sio.emit('send-rooms', UpdateRoomsMessage(json_rooms).tojson(), sid)
+        print(f"Sending rooms to ${sid}")
+
+    async def disconnect(self, sid: str):
+        if sid in self.connections:
+            self.connections.remove(sid)
+        else:
+            print(f"{sid} not present")
