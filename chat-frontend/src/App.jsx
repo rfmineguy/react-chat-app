f2faf4330adcd3f7cdf78d9f3d968052de50c70a
Riley Fischer <rfmineguy@gmail.com>
2025-08-05 09:17:54 -0700

    frontend: Setup socket endpoints on the frontend and their callbacks - onConnect : detects when the client successfully connects - onDisconnect : detects when the client successfully disconnects - onRooms : detects when the server has new rooms and refreshes the UI   accordingly - onChatlines : detects when the server has new chatlines in the room   and refreshes the UI accordingly - onJoinRoom : detects when the client receives an ack from the server   when a room is joined - Overall: Setup/Teardown of the endpoints

--- chat-frontend/src/App.jsx
+++ chat-frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState, useRef, createElement } from 'react'
-import reactLogo from './assets/react.svg'
 import { Chatbar } from './Chatbar.jsx'
-import { Chatline, ChatlineData } from './Chatline.jsx'
+import { Chatline } from './Chatline.jsx'
 import { RoomViewer } from './RoomViewer.jsx'
 import './App.css'
+import { socket } from './SocketInstance.js'
 
 const NewRoom = ({oncreateroom}) => {
   const inputref = useRef(null);
@@ -22,15 +22,58 @@ const NewRoom = ({oncreateroom}) => {
 };
 
 function App() {
+  const [rooms, setRooms] = useState([]);
+  const [joinedRoom, setJoinedRoom] = useState("");
+  const [isConnected, setIsConnected] = useState(socket.connected);
   const [chatlines, setChatlines] = useState([]);
 
   const submit = (content) => {
-    setChatlines(chatlines => [...chatlines, new ChatlineData(content)]);
+    socket.emit('addchatline', content)
   };
 
   const onjoinroom = (room) => {
-    console.log(`joining room ${room}`)
+    socket.emit('joinroom', {'room-name': room});
   };
+
+  const oncreateroom = (room) => {
+    socket.emit('createroom', {'room-name': room});
+  };
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+    function onRooms(rooms_) {
+      const rooms = rooms_.rooms.map(room => {
+        return JSON.parse(room);
+      });
+      setRooms(rooms);
+    }
+    function onChatLines(chatlines) {
+      setChatlines(chatlines.chatlines)
+    }
+    function onJoinRoom(room) {
+      if (room.room_name == joinedRoom) return;
+      setJoinedRoom(room.room_name);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('join-room', onJoinRoom);
+    socket.on('send-rooms', onRooms);
+    socket.on('update-chat', onChatLines);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('join-room', onJoinRoom);
+      socket.off('send-rooms', onRooms);
+      socket.off('update-chat', onChatLines);
+    };
+  }, []);
 
   return (
     <>
