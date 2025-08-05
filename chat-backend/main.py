4566bf8367d7f6de9394848fbfafa1c7e1ebeb5f
Riley Fischer <rfmineguy@gmail.com>
2025-08-05 09:03:25 -0700

    backend: setup the basic endpoints - connect : handles new connections and registers them - disconnect : handles leaving connections and unregisters them - createroom : handles requests to create new rooms - addchatline : handles requests to add new chatlines (will add to the   appropriate room based on sid) - joinroom : handles requests to move a user from one room to another

--- chat-backend/main.py
+++ chat-backend/main.py
@@ -9,3 +9,43 @@ app = FastAPI()
 app.mount("/", socketio.ASGIApp(sio))
 
 manager = ConnectionManager(sio)
+
+
+@sio.event
+async def connect(sid, environ):
+    print(f"Client connected: {sid}")
+    await manager.connect(sid)
+
+
+@sio.event
+async def joinroom(sid, data):
+    print(f"sid {sid} joining room {data}")
+    await manager.move_to_room(sid, data['room-name'])
+    manager.show_rooms()
+
+
+@sio.event
+async def createroom(sid, data):
+    if len(data) == 0:
+        print("Room empty. Not created")
+    else:
+        print(f"sid {sid} create room {data}")
+        await manager.create_room(sid, data['room-name'])
+        await manager.broadcast_rooms()
+
+
+@sio.event
+async def addchatline(sid, chatline):
+    await manager.addchatline(sid, chatline)
+
+
+@sio.event
+async def disconnect(sid):
+    print(manager.connections)
+    print(f"Client disconnected: {sid}")
+    await manager.disconnect(sid)
+
+
+@app.get("/")
+def read_root():
+    return {}
